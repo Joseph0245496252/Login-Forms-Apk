e6b4789a9fe048d69c6a1d5c3a5f073513a78823
Joseph Asante <freecoding1@gmail.com>
2020-04-21 10:17:12 +0000

    Logn in app rendered successfully

--- App.js
+++ App.js
@@ -1,19 +1,38 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+import LoginScreen from './LoginScreen'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+
+class App extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      emil:"",
+      password:""
+      // names:["Kwajo", "Kwame", "Kwasi", "Kofi", "Yaw"]
+       
+    }
+  }
+  
+  render() {
+    return (
+      <View style={styles.container}>
+        <LoginScreen />
+        
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    // flex: 1,
+    // backgroundColor: '#fff',
+    // alignItems: 'center',
+    // justifyContent: 'center',
   },
 });
+
+
+export default App; 
